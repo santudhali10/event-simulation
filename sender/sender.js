1c94ddce15bcacd18e5a7936aec07f24dd8a6620
santudhali <santu.d@een.com>
2024-10-09 15:53:29 +0530

    modified folder name

---
+++ sender/sender.js
@@ -0,0 +1,8 @@
+const path = require('path');
+
+
+const imagesFolderPath = path.join(__dirname, 'images');
+const configFilePath = path.join(__dirname, 'config.json');
+
+console.log('Images Folder Path:', imagesFolderPath);
+console.log('Config File Path:', configFilePath);
